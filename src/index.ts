a68077e5901bf947d359ca7041eec084852b1e10
nick-keller <nicolas.kllr@gmail.com>
2023-06-02 22:44:09 +0200

    Update typing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { TgglClient } from 'tggl-client'
+export type {
+  TgglFlagValue,
+  TgglFlagSlug,
+  TgglFlags,
+  TgglContext,
+} from 'tggl-client'
 export { TgglProvider, useTggl } from './TgglProvider'
 export { useFlag } from './useFlag'
